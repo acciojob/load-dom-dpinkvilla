72458a809d6a7088d34e1bba7e9b66f1b40238c8
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-22 12:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-	document.body.innerHTML += '<p>DOM load success</p>';
-});
+// document.addEventListener("DOMContentLoaded", function () {
+// 	document.body.innerHTML += '<p>DOM load success</p>';
+// });
+
+let line=document.createElement("p");
+line.innerText="DOM load success";
+
+document.addEventListener("DOMContentLoaded", ()=>{
+	
+	document.body.appendChild(line);
+})
